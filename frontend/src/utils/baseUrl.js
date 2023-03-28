327517b4ed425a0534767dea5dbceb2afbcfb653
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-03-28 11:18:09 +0600

    chnage url

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://fantastic-swimsuit-fox.cyclic.app"
+    ? "https://brainy-foal-threads.cyclic.app"
     : "http://localhost:5000";
 
 export default baseUrl;
